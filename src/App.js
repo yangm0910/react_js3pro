56f2f3d2baee6e316e7a4596c2df923efebcd4c3
yangm_1111 <yangm_1111@163.com>
2018-12-06 22:23:21 +0800

    use react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 import Header from './common/header';
 import { Provider } from 'react-redux';
+import { BrowserRouter, Route} from 'react-router-dom';
 import store from './store'
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
+        <div>
         <Header/>
+        <BrowserRouter>
+          <div>
+              <Route path="/" exact render={()=>("home")}/>
+              <Route path="/detail" render={()=>("detail")}/>
+          </div>
+        </BrowserRouter>
+        </div>
+
       </Provider>
     );
   }
